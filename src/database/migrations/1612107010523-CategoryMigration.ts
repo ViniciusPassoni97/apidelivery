e907393dce75eefc660e69d508e879c031fe1689
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-01-31 12:30:57 -0300

    Criando CategoriesMigration e ItensCategory

---
+++ src/database/migrations/1612107010523-CategoryMigration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CategoryMigration1612107010523 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
