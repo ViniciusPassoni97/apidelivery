fc26b2549741d9467cf82263e8a8dc82ef24651c
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-01-31 12:17:53 -0300

    Criando conexao com o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import route from './routes/index';
+import './database/connection';
 
 const app = express(); 
 
+app.use(express.json());
 app.use(route);
 
 app.listen(3333,()=>{
