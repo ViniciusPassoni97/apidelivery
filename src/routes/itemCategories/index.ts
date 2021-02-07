984237bd1364937415308d3857242e794e0c3699
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-06 23:14:45 -0300

    Primeiras configurações de multer

--- src/routes/itemCategories/index.ts
+++ src/routes/itemCategories/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import itemController from '../../controllers/item/index';
+import multer from 'multer';
+import ConfigMulter from '../../config/uploads';
 
 const route = express.Router();
+
+
+const upload = multer(ConfigMulter);
 
 route.get('/itemCategories',itemController.show);
 route.get('/itemCategories/:id',itemController.showById);
